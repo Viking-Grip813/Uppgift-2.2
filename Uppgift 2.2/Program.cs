7601184d672096e56102c4466f364d578f01003a
vist07001 <vist07001@E-PW05CGR8>
2024-10-09 12:28:13 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Uppgift2_2
+{ class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket längre hoppade Elin än Alma i meter?");
+            String Elinhoppadetext = Console.ReadLine();
+            double Elinhoppade = double.Parse(Elinhoppadetext);
+         
+        }
+    }
+}
